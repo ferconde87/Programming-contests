8ab599ca918fec5199c5b7aae308ba80d9bdfea3
Fernando Conde <ferconde87@gmail.com>
2018-05-28 23:01:36 -0300

    two-queens-problem

--- guide-to-competitive-programming/03 efficiency/examples/two-queens-problem.cpp
+++ guide-to-competitive-programming/03 efficiency/examples/two-queens-problem.cpp
@@ -4,7 +4,69 @@ using namespace std;
 
 #define cn(x) cout << #x << " = " << x << endl;
 
+int solve(int n){
+    int col[n];
+    int diag1[n+n-1];
+    int diag2[n+n-1];
+
+    for(int i = 0; i < n; i++){
+        col[i] = 0;
+    }
+    for(int i = 0; i < n+n-1; i++){
+        diag1[i] = diag2[i] = 0;
+    }
+
+    int ans = 0;
+    for(int x = 0; x < n; x++){
+        for(int y = 0; y < n; y++){
+            col[y] = diag1[x+y] = diag2[y-x+n-1] = 1;
+            for(int x2 = x+1; x2 < n; x2++){
+                for(int y2 = 0; y2 < n; y2++){
+                    if((col[y2] == 1) || (diag1[x2+y2] == 1) || (diag2[y2-x2+n-1] == 1)) continue;
+                    ans++;
+                }
+            }
+            col[y] = diag1[x+y] = diag2[y-x+n-1] = 0;
+        }
+    }
+    return ans;
+}
+
+//with bitset
+int solve2(int n){
+    bitset<200> col;
+    bitset<399> diag1;
+    bitset<399> diag2;
+    int ans = 0;
+    for(int x = 0; x < n; x++){
+        for(int y = 0; y < n; y++){
+            col[y] = diag1[x+y] = diag2[y-x+n-1] = true;
+            for(int x2 = x+1; x2 < n; x2++){
+                for(int y2 = 0; y2 < n; y2++){
+                    if(col[y2] || diag1[x2+y2] || diag2[y2-x2+n-1]) continue;
+                    ans++;
+                }
+            }
+            col[y] = diag1[x+y] = diag2[y-x+n-1] = false;
+        }
+    }
+    return ans;
+}
+
+
 int main(){
+
+    cout << "Brute force solution" << endl;
+    for(int i = 1; i <= 8; i++){
+        cout << i << " = " << solve(i);cout << endl;
+    }
+    cout << endl;
+
+    cout << "Brute force solution 2" << endl;
+    for(int i = 1; i <= 8; i++){
+        cout << i << " = " << solve2(i);cout << endl;
+    }
+    cout << endl;
 
     //O(N)
     // q(n)    = q(n − 1) + (2n − 1)(n 2 − 3(n − 1) − 1) − (n − 1)(n − 2)
