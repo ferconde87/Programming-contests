bea88150bdd0d68ea7df7b8b3732bf2d54a8e071
fer <ferconde87@gmail.com>
2018-10-06 10:21:23 -0300

    isPower

---
+++ codelab-facebook/ISPOWER/ispower.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+
+using namespace std;
+
+bool isPower(int A) {
+    if(A == 1) return true;
+    long long limitSearchSpace = sqrt(A);
+    for(long long a = 2; a <= limitSearchSpace; a++){
+        if(A % a == 0){
+            for(long long b = a; b <= A; b *= a){
+                if(b == A){
+                    return true;
+                } 
+            }
+        }
+    }
+    return false;
+}
+
+
+
+int main(){
+
+
+
+}
