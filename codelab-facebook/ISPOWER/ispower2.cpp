a8b1ffb81559c59584cbf19732dc54e1aef7fc9f
ferconde87 <ferconde87@gmail.com>
2018-10-06 18:58:16 -0300

    isPower & stepnum

---
+++ codelab-facebook/ISPOWER/ispower2.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+bool isPower(int x) {
+    if (x <= 1) return true;
+    for (int base = 2; base < x && base < INT_MAX / base; base++) {
+        int temp = base; 
+        while (temp <= x && temp < INT_MAX / base) {
+            temp *= base;
+            if (temp == x) return true;
+        }
+    }
+    return false;
+}
+
+int main(){
+
+
+    return 0;
+}
