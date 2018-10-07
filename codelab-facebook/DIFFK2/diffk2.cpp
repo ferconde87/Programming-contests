f4d7ec4afa3d474607252307ae99d58f7703dcc8
ferconde87 <ferconde87@gmail.com>
2018-10-07 11:44:47 -0300

    DIFFK2

---
+++ codelab-facebook/DIFFK2/diffk2.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int Solution::diffPossible(const vector<int> &A, int B) {
+    unordered_set<int> hashSet;
+    for(int x : A){
+        if(hashSet.find(x + B) != hashSet.end() || hashSet.find(x - B) != hashSet.end()){
+            return 1;
+        }
+        else{
+            hashSet.insert(x);
+        }
+    }
+    return 0;
+}
+
+
+int main(){
+
+
+    return 0;
+}
